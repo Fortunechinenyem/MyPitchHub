2c27b714e12dcc1931e3e09699ca163e21e82eab
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-15 16:34:03 +0100

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,11 +6,11 @@ const Hero = () => {
       <h2 className="">Welcome Back!</h2>
       <p className="">Login to Continue</p>
       <div className="login">
-        <p href="https://mypitchhub.com/" className="btn1 text-white">
+        <p className="btn1 text-white">
           <i className="fab fa-facebook"></i>Log in with Facebook
         </p>
 
-        <p href="https://mypitchhub.com/" className="btn2 text-white">
+        <p className="btn2 text-white">
           <i className="fab fa-google"></i> Log in with Google
         </p>
       </div>
@@ -18,13 +18,13 @@ const Hero = () => {
         className="w-50 mx-auto "
         style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
       >
-        <div style={{ flex: 1, height: "1px", backgroundColor: "black" }} />
+        <div style={{ flex: 1, height: "1px", backgroundColor: "#D8D8D8" }} />
 
         <div>
           <p style={{ width: "50px", textAlign: "center" }}>OR</p>
         </div>
 
-        <div style={{ flex: 1, height: "1px", backgroundColor: "black" }} />
+        <div style={{ flex: 1, height: "1px", backgroundColor: "#D8D8D8" }} />
       </div>
     </div>
   );
